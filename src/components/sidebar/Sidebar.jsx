6ab8b1d6aaff2db8cf87b62d3693830d94c4800e
olimov Amir <olimov.amir@mail.ru>
2023-10-11 09:29:37 +0300

    added changes to the sidebar file

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <section>
+      <div><h4>CATEGORY</h4></div>
+      <nav>
+      <ul>
+        <li>
+        <NavLink to={`/categories/${1}`} >Link</NavLink>
+        </li>
+      </ul>
+        
+      </nav>
+
+    </section>
   )
 }
 
